c6a739831df54e44b28dd2d49b199abd5ed88254
Gavin Yu <gavin.yu01@sap.com>
2020-10-12 17:02:29 +0800

    update test1

--- cal/test1.py
+++ cal/test1.py
@@ -1,15 +1,24 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
-
-msg = 'dddsss'
-print('msg: ', msg)
+msg = 'start py'
+print('msg:', msg)
 
 
 pd.set_option('display.max_columns', 100)
-df = pd.read_csv('cal/alternator_fan.csv')
-head=df.head()
+fan = pd.read_csv('cal/alternator_fan.csv')
+fan1 = fan.loc[:, ['material ID', 'diameter',
+                   'width', 'weight', 'price', 'rank']]
+print('fan1:\n', fan1)
+# head = fan.head()
+# data1 = fan.iloc[0:3, 2]
+# print('data1:', data1)
 
-print(head)
+# print(head)
 
+
+# df = pd.DataFrame(np.arange(0, 60, 2).reshape(10, 3), columns=list('abc'))
+# print('df:\n', df)
+# df1 = df.iloc[5:8, [1, 2]]
+# print('df1', df1)
