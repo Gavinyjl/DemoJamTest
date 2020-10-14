f9ac6fbf2a314d60e3e4a10e307423c84310ca09
Gavin Yu <gavin.yu01@sap.com>
2020-10-14 18:17:16 +0800

    Update test1.py

--- cal/test1.py
+++ cal/test1.py
@@ -10,29 +10,39 @@ print('msg:', msg)
 pd.set_option('display.max_columns', 100)
 fan = pd.read_csv('cal/alternator_fan.csv')
 fan1 = fan.loc[:, ['material ID', 'diameter',
-                   'width', 'weight', 'price', 'rank']]
+                   'width', 'weight', 'price', 'rank']]  # 选取需要的维度
 print('fan1:\n', fan1)
 
 print('fan1.shape:', fan1.shape)
 
+# fan2是将fan1的dataframe转化为二维矩阵
 fan2 = fan1.iloc[:, 1:6]
-print('fan2:', fan2)
+print('fan2:\n', fan2)
 print('fan2.mean0:', fan2.mean(axis=0))
 print('fan2.std0:', fan2.std(axis=0))
 
+# 标准化Z-Score，使均值在0附近，去除方差
+# fan2_scaled = ppc.scale(fan2)
+# print('fan2_scaled:\n', fan2_scaled)
+# print('fan2_scaled.mean:\n', fan2_scaled.mean(axis=0))
+# print('fan2_scaled.std:\n', fan2_scaled.std(axis=0))
 
-fan2_scaled = ppc.scale(fan2)
-print('fan2_scaled:\n', fan2_scaled)
-print('fan2_scaled.mean:\n', fan2_scaled.mean(axis=0))
-print('fan2_scaled.std:\n', fan2_scaled.std(axis=0))
+# 正则化normalization
+# fan3 = ppc.normalize(fan2, norm='l2')
+# print('fan3:\n', fan3)
 
-fan3 = ppc.normalize(fan2, norm='l2')
-print('fan3:\n', fan3)
-
+# MinMaxScaler将fan2归一化
 min_max_scaler = ppc.MinMaxScaler(feature_range=(0, 1))
 fan2_min_max = min_max_scaler.fit_transform(fan2)
 print('fan2_min_max.mean:\n', fan2_min_max.mean(axis=0))
 print('fan2_min_max.std:\n', fan2_min_max.std(axis=0))
+
+
+
+
+
+
+
 
 
 
