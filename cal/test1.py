120f153df02ba9c53d67d28b5011e53d5098a4e6
Gavin Yu <gavin.yu01@sap.com>
2020-10-14 10:14:31 +0800

    Update test1.py

--- cal/test1.py
+++ cal/test1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-
+import sklearn.metrics.pairwise as pw
+import sklearn.preprocessing as ppc
 
 msg = 'start py'
 print('msg:', msg)
@@ -12,15 +13,23 @@ fan1 = fan.loc[:, ['material ID', 'diameter',
                    'width', 'weight', 'price', 'rank']]
 print('fan1:\n', fan1)
 
-print('shape:', fan1.shape)
-# head = fan.head()
-# data1 = fan.iloc[0:3, 2]
-# print('data1:', data1)
+print('fan1.shape:', fan1.shape)
 
-# print(head)
+fan2 = fan1.iloc[:, 1:6]
+print('fan2:', fan2)
+print('fan2.mean0:', fan2.mean(axis=0))
+print('fan2.std0:', fan2.std(axis=0))
+
+
+fan2_scaled = ppc.scale(fan2)
+print('fan2_scaled:', fan2_scaled)
+print('fan2_scaled.mean:', fan2_scaled.mean(axis=0))
+print('fan2_scaled.std:', fan2_scaled.std(axis=0))
 
 
 # df = pd.DataFrame(np.arange(0, 60, 2).reshape(10, 3), columns=list('abc'))
 # print('df:\n', df)
+# print('df.mean0:\n', df.mean(axis=0))
+# print('df.mean1:\n', df.mean(axis=1))
 # df1 = df.iloc[5:8, [1, 2]]
 # print('df1', df1)
