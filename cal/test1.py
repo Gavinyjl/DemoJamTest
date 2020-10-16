066b7d5e93d090bdd0398c4f1c00c485f657e9aa
Gavin Yu <gavin.yu01@sap.com>
2020-10-16 18:51:33 +0800

    Update test1.py

--- cal/test1.py
+++ cal/test1.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sklearn.metrics.pairwise as pw
 import sklearn.preprocessing as ppc
+import random
 
 msg = 'start py'
 print('msg:', msg)
@@ -34,8 +35,25 @@ print('fan2.std0:', fan2.std(axis=0))
 # MinMaxScaler将fan2归一化
 min_max_scaler = ppc.MinMaxScaler(feature_range=(0, 1))
 fan2_min_max = min_max_scaler.fit_transform(fan2)
-print('fan2_min_max.mean:\n', fan2_min_max.mean(axis=0))
-print('fan2_min_max.std:\n', fan2_min_max.std(axis=0))
+print('fan2_min_max:\n', fan2_min_max)  # 平均数
+print('fan2_min_max.mean:\n', fan2_min_max.mean(axis=0))  # 平均数
+print('fan2_min_max.std:\n', fan2_min_max.std(axis=0))  # 标准差
+
+user_prefer = []
+for i in range(10000):
+    num = random.randint(0, 20)
+    user_prefer.append(num)
+
+
+print('user_prefer:', user_prefer)
+up_array = np.array(user_prefer)
+print('up_array:\n', up_array)
+up_array = up_array/up_array.sum()
+print('up_array回归化:\n', up_array)
+
+
+user_profile = up_array.dot(fan2_min_max)
+print('user_profile:\n', user_profile)
 
 
 # df = pd.DataFrame(np.arange(0, 60, 2).reshape(10, 3), columns=list('abc'))
