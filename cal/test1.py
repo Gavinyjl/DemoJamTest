90db87f2c53350a9df09269a39a8702f587b5ac9
Gavin Yu <gavin.yu01@sap.com>
2020-10-19 18:31:07 +0800

    update plt

--- cal/test1.py
+++ cal/test1.py
@@ -4,9 +4,20 @@ import sklearn.metrics.pairwise as pw
 import sklearn.preprocessing as ppc
 import random
 import scipy as sp
+from matplotlib import pyplot as plt
 
 msg = 'start py'
 print('msg:', msg)
+
+np.set_printoptions(threshold=np.inf, precision=3)
+
+
+def to_sum_one(arr):
+    arr = np.array(arr)
+    print('arr的类型：（函数内）', type(arr))
+    arr = arr/arr.sum()
+    print("\n归一化后的矩阵：", arr)
+    return(arr)
 
 
 pd.set_option('display.max_columns', 100)
@@ -18,10 +29,19 @@ print('fan1:\n', fan1)
 print('fan1.shape:', fan1.shape)
 
 # fan2是将fan1的dataframe转化为二维矩阵
+# def df_to_matrix(df):
+#     arr=df.
 fan2 = fan1.iloc[:, 1:6]
 print('fan2:\n', fan2)
 print('fan2.mean0:', fan2.mean(axis=0))
 print('fan2.std0:', fan2.std(axis=0))
+
+# 绘制fan2图像
+fig = plt.figure()
+index = [i for i in range(10000)]
+plt.plot(index, fan2['diameter'], '.')
+# plt.show()
+
 
 # 标准化Z-Score，使均值在0附近，去除方差
 # fan2_scaled = ppc.scale(fan2)
@@ -46,21 +66,41 @@ user_prefer = []
 #     num = random.randint(0, 20)
 #     user_prefer.append(num)
 
-b = sp.sparse.rand(1, 10000, 0.005)
-print('随机矩阵：', b)
+row_num = 1
+col_num = 10000
+ele_num = 500
+
+user_prefer = [row_num-1 for _ in range(col_num)]
+# print('row_arr', row_arr)
+row_seq = [i for i in range(0, col_num)]
+insert_arr = random.sample(row_seq, 100)
+print('\ninsert_arr', insert_arr)
+for i in range(len(insert_arr)):
+    user_prefer[insert_arr[i]] = random.randint(0, 5)
+print('\nuser_prefer插值后', user_prefer)
+
+# print('arr的类型：（函数前）', type(user_prefer))
+user_prefer = to_sum_one(user_prefer)
+# print('arr的类型：（函数后）', type(user_prefer))
 
 
-print('user_prefer:', user_prefer)
-up_array = np.array(user_prefer)
-print('up_array:\n', up_array)
-up_array = up_array/up_array.sum()
-print('up_array回归化:\n', up_array)
+# b = sp.sparse.rand(1, 10000, 0.005)
+# print('随机矩阵：', b)
 
 
-user_profile = up_array.dot(fan2_min_max)
+# print('user_prefer:', user_prefer)
+# up_array = np.array(user_prefer)
+# print('up_array:\n', up_array)
+# up_array = up_array/up_array.sum()
+# print('up_array回归化:\n', up_array)
+
+
+# user_profile由user_prefer和fan2_min_max矩阵相乘得到
+user_profile = user_prefer.dot(fan2_min_max)
 print('user_profile:\n', user_profile)
 
 
+# df例子
 # df = pd.DataFrame(np.arange(0, 60, 2).reshape(10, 3), columns=list('abc'))
 # print('df:\n', df)
 # print('df.mean0:\n', df.mean(axis=0))
